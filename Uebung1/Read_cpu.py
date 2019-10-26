7b8ada5a072a56534c044c44d7a3659fb171fcfd
Raphael1310 <raphael.pliessnig@chello.at>
2019-10-26 13:32:51 +0200

    blabla
    bla

---
+++ Uebung1/Read_cpu.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+
+
+
+
+cpufilepfad = r"C:\Users\Raphael\Desktop\UNI\Daten_2.5\Uebung1\t1.txt"  #Pfad durch echten Pfad ersetzen
+cpufile=open(cpufilepfad,"r")
+test = cpufile.readlines()
+a=5
+print(test)
+print("test")
+cpufile.close()
+    
+#def readcpu(pfad):
+
+
+
+
+
+
+
+
+
+    
+    
