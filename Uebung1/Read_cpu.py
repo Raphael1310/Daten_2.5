478968760103bca2f0f577e409394f7a7a8d1077
Raphael1310 <raphael.pliessnig@chello.at>
2019-10-27 19:15:52 +0100

    fertig
    fertig

--- Uebung1/Read_cpu.py
+++ Uebung1/Read_cpu.py
@@ -1,26 +1,54 @@
 # -*- coding: utf-8 -*-
+import time
+import random
+
+class Cpustats:
+    def __init__(self, name):
+        self.name = name
+        self.oldstat = float(0)
+        self.newstat = float(0)
+
+def UpdateCpustats(cpustatlist, cpuread):
+    for x,stats in enumerate(cpustatlist):
+        stats.oldstat = stats.newstat
+        stats.newstat = float(cpuread[x+1])
+
+def Readcpu():
+    cpufilepfad1 = [r"C:\Users\Raphael\Desktop\UNI\Daten_2.5\Uebung1\t1.txt",r"C:\Users\Raphael\Desktop\UNI\Daten_2.5\Uebung1\t2.txt"]
+    cpufilepfad = cpufilepfad1[random.randint(0,1)]
+    cpufile=open(cpufilepfad,"r")
+    cpuread = cpufile.readlines(1)[0].split()
+    cpufile.close()
+    return cpuread
+
+Cpustatlist= []
+names = ["user","nice","system", "idle", "iowait", "irq", "softirq"]
+for name in names:
+    Cpustatlist.append(Cpustats(name))
 
 
+cpuread=Readcpu()
 
+UpdateCpustats(Cpustatlist, cpuread)
 
-
-cpufilepfad = r"C:\Users\Raphael\Desktop\UNI\Daten_2.5\Uebung1\t1.txt"  #Pfad durch echten Pfad ersetzen
-cpufile=open(cpufilepfad,"r")
-test = cpufile.readlines()
-a=5
-print(test)
-print("test")
-cpufile.close()
+failsafe = 0 
+while True: 
+    cpuread = Readcpu()
+    UpdateCpustats(Cpustatlist,cpuread)
     
-#def readcpu(pfad):
-
-
-
-
-
-
-
-
-
+    for cpustat in Cpustatlist:
+        aenderung = 0
+        if cpustat.oldstat != 0:
+            aenderung = (cpustat.oldstat - cpustat.newstat)/cpustat.oldstat
+        print(cpustat.name + " = " + str(aenderung)  + "%")
     
     
+    failsafe +=1
+    if failsafe == 5 :
+        break
+    print("---------------------------------------------------")
+    time.sleep(5)
+
+
+
+
