7f25de128a7f55f7b5dbe1f68f51bc4c8d7aee76
Thomas Köckeis <tkoeckeis@gmx.at>
2019-11-06 11:40:24 +0100

    SADH

---
+++ Uebung1/Sudoku/Sudoku/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace Sudoku
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.b_Reset = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.b_Laden = new System.Windows.Forms.Button();
+            this.b_Solve = new System.Windows.Forms.Button();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.b_Loadfile = new System.Windows.Forms.Button();
+            this.oFD1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgv
+            // 
+            this.dgv.AllowUserToAddRows = false;
+            this.dgv.AllowUserToDeleteRows = false;
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8,
+            this.Column9});
+            this.dgv.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgv.Location = new System.Drawing.Point(0, 0);
+            this.dgv.MultiSelect = false;
+            this.dgv.Name = "dgv";
+            this.dgv.RowHeadersWidth = 50;
+            this.dgv.Size = new System.Drawing.Size(498, 326);
+            this.dgv.TabIndex = 0;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.dgv);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.b_Loadfile);
+            this.splitContainer1.Panel2.Controls.Add(this.b_Reset);
+            this.splitContainer1.Panel2.Controls.Add(this.button2);
+            this.splitContainer1.Panel2.Controls.Add(this.button1);
+            this.splitContainer1.Panel2.Controls.Add(this.b_Laden);
+            this.splitContainer1.Panel2.Controls.Add(this.b_Solve);
+            this.splitContainer1.Size = new System.Drawing.Size(498, 383);
+            this.splitContainer1.SplitterDistance = 326;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // b_Reset
+            // 
+            this.b_Reset.Location = new System.Drawing.Point(236, 3);
+            this.b_Reset.Name = "b_Reset";
+            this.b_Reset.Size = new System.Drawing.Size(106, 22);
+            this.b_Reset.TabIndex = 4;
+            this.b_Reset.Text = "Reset";
+            this.b_Reset.UseVisualStyleBackColor = true;
+            this.b_Reset.Click += new System.EventHandler(this.b_Reset_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 28);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 22);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Loop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.b_Loop_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(124, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 22);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Load Manuell";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.b_load_manuell_Click);
+            // 
+            // b_Laden
+            // 
+            this.b_Laden.Location = new System.Drawing.Point(124, 2);
+            this.b_Laden.Name = "b_Laden";
+            this.b_Laden.Size = new System.Drawing.Size(106, 22);
+            this.b_Laden.TabIndex = 1;
+            this.b_Laden.Text = "Load Default";
+            this.b_Laden.UseVisualStyleBackColor = true;
+            this.b_Laden.Click += new System.EventHandler(this.b_Laden_Click);
+            // 
+            // b_Solve
+            // 
+            this.b_Solve.Location = new System.Drawing.Point(12, 2);
+            this.b_Solve.Name = "b_Solve";
+            this.b_Solve.Size = new System.Drawing.Size(106, 22);
+            this.b_Solve.TabIndex = 0;
+            this.b_Solve.Text = "Solve";
+            this.b_Solve.UseVisualStyleBackColor = true;
+            this.b_Solve.Click += new System.EventHandler(this.b_Solve_Click);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "0";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 30;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "1";
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 30;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "2";
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 30;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "3";
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 30;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "4";
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 30;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "5";
+            this.Column6.Name = "Column6";
+            this.Column6.Width = 30;
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "6";
+            this.Column7.Name = "Column7";
+            this.Column7.Width = 30;
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "7";
+            this.Column8.Name = "Column8";
+            this.Column8.Width = 30;
+            // 
+            // Column9
+            // 
+            this.Column9.HeaderText = "8";
+            this.Column9.Name = "Column9";
+            this.Column9.Width = 30;
+            // 
+            // b_Loadfile
+            // 
+            this.b_Loadfile.Location = new System.Drawing.Point(236, 28);
+            this.b_Loadfile.Name = "b_Loadfile";
+            this.b_Loadfile.Size = new System.Drawing.Size(106, 22);
+            this.b_Loadfile.TabIndex = 5;
+            this.b_Loadfile.Text = "Load File";
+            this.b_Loadfile.UseVisualStyleBackColor = true;
+            this.b_Loadfile.Click += new System.EventHandler(this.b_Loadfile_Click);
+            // 
+            // oFD1
+            // 
+            this.oFD1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(498, 383);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Sudoku";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button b_Solve;
+        private System.Windows.Forms.Button b_Laden;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button b_Reset;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+        private System.Windows.Forms.Button b_Loadfile;
+        private System.Windows.Forms.OpenFileDialog oFD1;
+    }
+}
+
---
+++ Uebung1/Sudoku/Sudoku/Form1.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sudoku
+{
+    public partial class Form1 : Form
+    {
+        public int[,] Werte = new int[9,9];//row und dann spalte  fertige Werte
+        public List<int>[][] posiblWerte;//enthät alle noch möglichen werten index1= row index2 = spalte
+       
+        #region "Events"
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                dgv.Rows.Add();
+                dgv.Rows[i].HeaderCell.Value = i.ToString();
+            }
+
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void b_Laden_Click(object sender, EventArgs e)
+        {
+            Load_Standard_Sudokuwerte();
+            Show_werte();
+        }
+        private void b_Solve_Click(object sender, EventArgs e)
+        {
+            Load_posWerte();
+            for (int loops = 0; loops < 65; loops++)
+            {
+                for (int i = 0; i < 9; i++)
+                {
+                    for (int j = 0; j < 9; j++)
+                    {
+                        Update_posWerte(i, j);
+                    }
+                }
+            }
+            Show_werte();
+        }
+        private void b_Loop_Click(object sender, EventArgs e)
+        {
+            for (int loops = 0; loops < 100; loops++)
+            {
+                for (int i = 0; i < 9; i++)
+                {
+                    for (int j = 0; j < 9; j++)
+                    {
+                        Update_posWerte(i, j);
+                    }
+                }
+            }
+            Show_werte();
+        }
+        private void b_load_manuell_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    int resutint;
+                    bool resultb = int.TryParse(dgv[j, i].Value.ToString(),out resutint);
+                    if (resultb)
+                    {
+                        if (resutint > -1 && resutint < 10)
+                        {
+                            Werte[i, j] = resutint;
+                        }
+                        else
+                        {
+                            MessageBox.Show("Unültige Feldeingabe: " + dgv[j, i].Value.ToString() + " bei Index:" + i.ToString() + ";" + j.ToString());
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        if (dgv[j, i].Value.ToString() == "")
+                        {
+                            Werte[i, j] = 0;
+                        }
+                        else
+                        {
+                            MessageBox.Show("Unültige Feldeingabe: " + dgv[j, i].Value.ToString() + " bei Index:" + i.ToString() + ";" + j.ToString());
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+        private void b_Reset_Click(object sender, EventArgs e)
+        {
+            Load_posWerte();
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    Werte[i, j] = 0;
+                }
+            }
+            Show_werte();
+        }
+        #endregion
+
+        #region "Hilfsfunktionen"
+        private void Load_Standard_Sudokuwerte()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    Werte[i, j] = 0;
+                }
+            }
+            Werte[0, 2] = 8;
+            Werte[0, 6] = 9;
+            Werte[1, 1] = 6;
+            Werte[1, 2] = 2;
+            Werte[1, 4] = 9;
+            Werte[1, 5] = 8;
+            Werte[1, 7] = 3;
+            Werte[2, 2] = 9;
+            Werte[2, 3] = 1;
+            Werte[2, 4] = 3;
+            Werte[2, 7] = 8;
+
+            Werte[3, 0] = 9;
+            Werte[3, 2] = 3;
+            Werte[3, 5] = 7;
+            Werte[3, 7] = 6;
+            Werte[3, 8] = 5;
+            Werte[4, 4] = 1;
+            Werte[5, 0] = 2;
+            Werte[5, 1] = 5;
+            Werte[5, 3] = 6;
+            Werte[5, 6] = 7;
+            Werte[5, 8] = 1;
+
+            Werte[6, 0] = 7;
+            Werte[6, 2] = 6;
+            Werte[6, 4] = 4;
+            Werte[6, 5] = 9;
+            Werte[6, 7] = 5;
+            Werte[6, 8] = 8;
+            Werte[7, 0] = 3;
+            Werte[7, 3] = 8;
+            Werte[7, 5] = 1;
+            Werte[7, 8] = 9;
+            Werte[8, 0] = 8;
+            Werte[8, 1] = 9;
+            Werte[8, 2] = 1;
+            Werte[8, 3] = 2;
+            Werte[8, 4] = 5;
+            Werte[8, 8] = 4;
+        }
+        private void Show_werte()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    if (Werte[i, j] != 0)
+                    {
+                        dgv.Rows[i].Cells[j].Value = Werte[i, j];
+                    }
+                    else
+                    {
+                        dgv.Rows[i].Cells[j].Value = "";
+                    }
+                }
+            }
+        }
+        private void Load_posWerte()
+        {
+            posiblWerte = new List<int>[9][];
+            for (int i = 0; i < 9; i++)
+            {
+                posiblWerte[i] = new List<int>[9];
+                for (int j = 0; j < 9; j++)
+                {
+                    posiblWerte[i][j] = new List<int>();
+                    for (int k = 1; k < 10; k++)
+                    {
+                        int saver = saver = k;
+                        posiblWerte[i][j].Add(saver);
+                    }
+                }
+            }
+        }
+        private void Update_posWerte(int row, int coloumn)
+        {
+            if (Werte[row, coloumn] > 0)
+            {
+                return;
+            }
+
+            //Box
+            int corerow;
+            int corecol;
+            if (row > 5)
+            {
+                corerow = 7;
+            }
+            else if (row < 3)
+            {
+                corerow = 1;
+            }
+            else
+            {
+                corerow = 4;
+            }
+
+            if (coloumn > 5)
+            {
+                corecol = 7;
+            }
+            else if (coloumn < 3)
+            {
+                corecol = 1;
+            }
+            else
+            {
+                corecol = 4;
+            }
+
+            for (int i = -1; i < 2; i++)
+            {
+                for (int j = -1; j < 2; j++)
+                {
+                    if (Werte[corerow + i, corecol + j] != 0)
+                    {
+                        posiblWerte[row][coloumn].RemoveAll(value => value == Werte[corerow + i, corecol + j]);
+                    }
+                }
+            }
+
+            //Row
+            for (int i = 0; i < 9; i++)
+            {
+                if (Werte[row, i] != 0)
+                {
+                    posiblWerte[row][coloumn].RemoveAll(value => value == Werte[row, i]);
+                }
+            }
+
+            //Spalte
+            for (int i = 0; i < 9; i++)
+            {
+                if (Werte[i, coloumn] != 0)
+                {
+                    posiblWerte[row][coloumn].RemoveAll(value => value == Werte[i, coloumn]);
+                }
+            }
+
+            if (posiblWerte[row][coloumn].Count == 1)
+            {
+                int test = posiblWerte[row][coloumn][0];
+                Werte[row, coloumn] = posiblWerte[row][coloumn][0];
+                posiblWerte[row][coloumn].RemoveAt(0);
+                //Show_werte();
+                //Application.DoEvents();
+            }
+        }
+
+        #endregion
+
+        private void b_Loadfile_Click(object sender, EventArgs e)
+        {
+            oFD1.Multiselect = false;
+            oFD1.Title = "Choose File to open";
+             while (!System.IO.File.Exists(oFD1.FileName))
+                {
+                    oFD1.ShowDialog();
+                }
+            System.IO.StreamReader sr = new System.IO.StreamReader(oFD1.FileName);
+            int row = 0;
+            while (row < 8 && sr.Peek() != -1)
+            {
+                string zeile = sr.ReadLine();
+                if (!string.IsNullOrEmpty(zeile))
+                {
+                    string[] values = zeile.Split(' ');
+                    if (values.Count() == 11)
+                    {
+                        int steps = 0;
+                        for ( int i = 0;i<9;i++)
+                        {
+                            if (i == 3 || i == 6)
+                            {
+                                steps++;
+                            }
+                            Werte[row, i] = Convert.ToInt32(values[i+steps]);
+                        }
+                        row++;
+                    }
+                }
+               
+            }
+            Show_werte();
+        }
+    }
+}
