35a9df5917506e465c08307f1f226531f4d401b0
Raphael1310 <raphael.pliessnig@chello.at>
2019-11-24 19:00:47 +0100

    Uebung 4 done

--- Uebung4/Wuerfel_client.py
+++ Uebung4/Wuerfel_client.py
@@ -4,8 +4,9 @@
 """ssl_client.py host port message cert
 """
 
-import sys,string,socket,time
+import sys,socket,time
 import numpy as np
+from tabulate import tabulate
 # 
 # if len(sys.argv) != 4:
 #    print  "Usage: %s host port message" % sys.argv[0]
@@ -16,9 +17,34 @@ class Wuerfel():
     def __init__(self, name):
         self.name = name 
         self.wuerfeldata = []
-        self.std = None
-        self.mean = None
-        self.occurence = []
+        self.binstd = None
+        self.binmean = None
+        self.binoccurence = []
+        self.bindeviationfrommean=[]
+        self.neighbourcorrelation=None
+    def CalcBinomialStatistics(self):
+        self.mean = np.mean(self.wuerfeldata)
+        self.std = np.std(self.wuerfeldata)
+        for x in range(1,7):
+            self.binoccurence.append(np.sum(self.wuerfeldata == x))
+            self.bindeviationfrommean.append((self.binoccurence[x-1]-binmean)/binstd)
+       
+    def CalcNeighbourcorrelation(self):
+        self.neighbourcorrelation = np.zeros((6,6))
+        for i in range(1,self.wuerfeldata.size):
+            self.neighbourcorrelation[self.wuerfeldata[i-1]-1,self.wuerfeldata[i]-1]+=1
+            
+        
+def printdata(filename, wuerfeldata):
+    with open(filename, 'w') as f:
+        for w in wuerfeldata:
+            f.writelines("name= {}\n".format(w.name))
+            for i,zahl in enumerate(w.binoccurence):
+                f.writelines("number = {} | occurence={} | deviation in std={}\n".format(i+1,zahl,w.bindeviationfrommean[i]))
+            f.writelines("Neighbourcorrelation\n")
+            headers= [1,2,3,4,5,6]
+            f.writelines(tabulate(w.neighbourcorrelation,headers,showindex=range(1,7)))
+            f.writelines("\n---------------------------------------------\n")
         
 
 def ConnectToServer(s):
@@ -41,33 +67,38 @@ def SendPlusRecive(s,message):
     data = s.recv(4096)
     return data
 
+
+
+
 KENNZAHLEN = [0, 1, 2, 3, 4, 5, 6, 71, 10, 11, 12, 13, 80, 20, 21, 22, 23, 100, 90, 70]
 NUMBEROFTHROWS = 1000
-message = "help".encode("utf-8")
+binmean=NUMBEROFTHROWS/6
+binstd=np.sqrt(binmean*(1-5/6))
+outputfile=r'C:\Users\Raphael\Desktop\UNI\Daten_2.5\Git_Daten\Uebung4\output.txt'
 
+ 
 # Oeffnen des Sockets
 # Auch der client muss einen socket oeffnen
-
-Wuerfelarray=[]
-
-
-for i,zahl in enumerate(KENNZAHLEN):
-    try:
-       s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    except socket.error:
-       s = None
-    ConnectToServer(s)
-    Wuerfelarray.append(Wuerfel(zahl))
-    message = "throw {} {}".format(NUMBEROFTHROWS,zahl).encode("utf-8")
-    with Wuerfelarray[i-1] as w: 
-        w.wuerfeldata = np.asarray(list(SendPlusRecive(s,message)))[:-1]-48
-        w.mean = np.mean(w.wuerfeldata)
-        w.std = np.std(w.wuerfeldata)
-        for x in range(6):
-            w.occurence.append(np.sum(w.wuerfeldata == x))
-        print("name = {} || mean={} || std = {}".format(w.name,w.mean,w.std))
-        print(w.occurence)
-    s.close()
+if __name__== "__main__":
+    Wuerfelarray=[]
+    
+    for i,zahl in enumerate(KENNZAHLEN):
+        try:
+           s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        except socket.error:
+           s = None
+        ConnectToServer(s)
+        Wuerfelarray.append(Wuerfel(zahl))
+        message = "throw {} {}".format(NUMBEROFTHROWS,zahl).encode("utf-8")
+        Wuerfelarray[i].wuerfeldata = np.asarray(list(SendPlusRecive(s,message)))[:-1]-48
+        s.close()
+    
+    
+    for w in Wuerfelarray:
+        w.CalcBinomialStatistics()
+        w.CalcNeighbourcorrelation()
+           
+    printdata(outputfile,Wuerfelarray)
     
 
     
